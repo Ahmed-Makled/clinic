6ca367924a0fbc247369f57f130c9d3129b9db05
Ahmed Makled <36972797+Ahmed-Makled@users.noreply.github.com>
2025-04-22 01:12:39 +0200

    refactor: Remove LoginController routes from web.php

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,4 @@ Route::post('/contact', [ContactController::class, 'store'])->name('contact.stor
 Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register']);
 
-Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('login', [LoginController::class, 'login']);
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-
 Route::get('/about', [AboutController::class, 'index'])->name('about');
